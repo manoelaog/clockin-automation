5e6bd713565f48b58c4243ae3abf0d8b7074cbf2
Manoela Oliveira Gonzaga <manoela.gonzaga@totvs.com.br>
2023-04-27 20:19:56 -0300

    alteração cypress.config

--- backoffice/cypress.config.ts
+++ backoffice/cypress.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   env: {
     email: 'manoela.gonzaga@totvs.com.br',
     emailFake: 'login.teste@totvs.com.br',
-    password: '@19July#Totvs',
+    password: 'password',
   },
   e2e: {
     baseUrl: 'https://totvsclockin.carol.ai/totvsrm/apps/dev/totvsrmbocdsdm/0.0.1/index.html',
